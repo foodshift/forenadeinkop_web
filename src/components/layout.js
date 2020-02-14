09193d1f72e1ee9d1fb73851f14afa303d598168
Kristofer Lund <kristofer@fmckl.se>
2020-02-14 23:30:25 +0100

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,24 +23,28 @@ const Layout = ({ children }) => {
     }
   `)
 
+  // < Header siteTitle = { data.site.siteMetadata.title } /> -->
+  //   <footer>
+  //   © {new Date().getFullYear()},
+  //     {` `}
+  //   <a href="https://foodshift.se">Food Shift</a>
+  // </footer>
+
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <div className='w-100 bt b--black-10 bg-white sans-serif'>
+      <main>{children}</main>
+      <footer class="bg-white black-70 ph3 ph5-ns pv5 pv6-ns bt b--black-10">
+        <div class="mw9 center">
+          <div class="mb5 lh-copy">
+            <a class="black-70 link hover-blue b dib mr3 mb3" href="https://foodshift.se" title="Food Shift">
+              Food Shift</a>
+          </div>
+        </div>
+        <p class="f6 measure copy lh-copy">
+          Food Shift is an open collective nourishing and connecting people, projects and organisations in emerging regenerative food systems.
+  </p>
+      </footer>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { css, jsx } from '@emotion/core'
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const color = 'white'
+
+const Section = (props) => {
+  return (
+    <section className='bg-green black-80 pv5 pv6-ns bb b--black-10'>
+      <div className='ph3 ph5-ns'>
+        {props.children}
+
+      </div>
+    </section>
+  )
+}
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <Section>
+      <div class="center mw9">
+        <h3 class="f5 fw6 ttu tracke">Förenade Inköp</h3>
+      </div>
+
+      <div class="cf w-100 center mw9">
+        <blockquote class="fl w-100 mh0 mb2 mb3-ns border-box">
+          <p class="f4 f2-m mt0 db fl w-100 f-subheadline-l lh-copy lh-title-l measure mb4 fw6">
+            Kooperativ livsmedelshandel för lokal utveckling.
+              </p>
+          <p class="fl w-100 w-50-l mh0 mt0 pr0 pr3-l measure lh-copy f5 f4-l">
+            Ett informationsprojekt inom ramen för Jordbruksverkets satsning på korta livsmedelskedjor. Projektet har som övergripande mål att bidra till att fler inköpsföreningar startas samt att befintliga inköpsföreningar inleder djupare samarbeten.
+              </p>
+
+        </blockquote>
+      </div>
+    </Section>
   </Layout>
 )
 
