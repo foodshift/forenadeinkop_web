433b0b93e86d1cdca1222495dde1569fa1c46a0a
Kristofer Lund <kristofer@fmckl.se>
2020-03-26 14:40:47 +0100

    Blogg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,54 @@
 module.exports = {
   siteMetadata: {
-    title: `Förenade Inköp`,
-    description: `Ett informationsprojekt inom ramen för Jordbruksverkets satsning på korta livsmedelskedjor. Projektet har som övergripande mål att bidra till att fler inköpsföreningar startas samt att befintliga inköpsföreningar inleder djupare samarbeten.`,
-    author: `@food_shift`,
+    title: 'Förenade Inköp',
+    description: 'Ett informationsprojekt inom ramen för Jordbruksverkets satsning på korta livsmedelskedjor. Projektet har som övergripande mål att bidra till att fler inköpsföreningar startas samt att befintliga inköpsföreningar inleder djupare samarbeten.',
+    author: '@food_shift'
   },
   plugins: [
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#19a974`,
-        theme_color: `#19a974`,
-        display: `minimal-ui`,
-        icon: `src/images/seedling-solid.svg`, // This path is relative to the root of the site.
-      },
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#19a974',
+        theme_color: '#19a974',
+        display: 'minimal-ui',
+        icon: 'src/images/seedling-solid.svg' // This path is relative to the root of the site.
+      }
     },
+    {
+      resolve: 'gatsby-source-custom-api',
+      options: {
+        url: 'https://foodshift.se/t/49/posts.json',
+        rootKey: 'blog',
+        schemas: {
+          blog: `
+                  post_stream: post_stream
+              `,
+          post_stream: `
+                  posts: [post]
+              `,
+          post: `
+                  id: Int
+                  cooked: String
+          `
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import { BorderSection, Section } from '../components/Section'
+import Seo from '../components/seo'
+import sanitizeHtml from 'sanitize-html'
+import formatDistance from 'date-fns/formatDistance'
+import parseISO from 'date-fns/parseISO'
+import sv from 'date-fns/locale/sv'
+import '../css/local.css'
+
+const formatExcerpt = (excerpt) => excerpt.replace(/\n/g, '<br/>')
+const formatDate = (datestring) => formatDistance(parseISO(datestring), Date.now(), { locale: sv })
+const formatAvatar = (avatar_template) => <img
+  src={`https://foodshift.se${avatar_template.replace('{size}', '100')}`} className='br-100 dib fl mr2'
+  alt='avatar' style={{ width: 45 }}
+/>
+
+const sanitize = (dirty) => sanitizeHtml(dirty, {
+  allowedTags: ['b', 'i', 'em', 'strong', 'a', 'img'],
+  nonTextTags: ['style', 'script', 'textarea', 'noscript', 'aside'],
+  allowedClasses: {
+    img: ['emoji']
+  }
+})
+
+const Post = ({ data }) => {
+  return (
+    <Layout>
+      <Seo title='Blog' />
+      <Section textColor='green'>
+        <div className='center mw9 pt5 pt6-l mb4 mb5-l'>
+          <h3 className='f5 fw6 ttu tracke'>Förenade Inköp</h3>
+        </div>
+
+        <div className='fl w-100 mh0 mb3 mb4-l'>
+          <p className='f2 mt0 db fl w-100 f-subheadline-l lh-copy lh-title-l measure mb4 mb4-l fw6'>
+            Blogg.
+          </p>
+        </div>
+      </Section>
+      {
+        data.blog.post_stream.posts.reverse().map((post, index) => (
+
+          <BorderSection key={index}>
+            <div className='fl w-100 mh0 mb2 mb3-ns pt2'>
+              <div className='fl w-100 w-50-l mh0 mt0 pr0 pr3-l measure lh-copy f5 f4-l'>
+
+                <p className='mt2 mb3' dangerouslySetInnerHTML={{ __html: sanitize(post.cooked) }} />
+                <p>–</p>
+                <div className='f5'>
+                  <a href={`https://foodshift.se/u/${post.username}`}>
+                    {formatAvatar(post.avatar_template)}
+                  </a>
+                  {post.name}{' '}<a href={`https://foodshift.se/u/${post.username}`}>@{post.username}</a>
+                  <br />
+                  Skrev inlägget {formatDate(post.created_at)} sedan
+                </div>
+
+              </div>
+            </div>
+          </BorderSection>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    blog {
+      post_stream {
+        posts {
+          username
+          name
+          created_at
+          cooked
+          avatar_template
+        }
+      }
+    }
+  }
+`
+
+export default Post
